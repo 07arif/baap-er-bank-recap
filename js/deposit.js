8b90390c743523cc7ee86f64ac80a4c31de2e6c4
07arif <ai.arif0711@gmail.com>
2022-08-22 06:17:49 +0600

    js deposit balance  added

--- js/deposit.js
+++ js/deposit.js
@@ -1,14 +1,37 @@
 /*
 1. add event listner to the deposit button
 2.get deposit amount from the deposit input field.
-
-
-
+2-5. convert string deposit amount to number type
+3. clear the deposit input field after geeting teh value
+4.get the previous deposit total;
 
 */
-document.getElementById('btn-deposit').addEventListener('click', function () {
-    const depositAmount = document.getElementById('deposit-field');
-    const newDepositAmountString = depositAmount.value;
-    console.log(newDepositAmountString)
 
+// step-1
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    //step-2
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    //step-3
+    depositField.value = '';
+
+    //step-4
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    previousDepositTotal = parseFloat(previousDepositTotalString);
+
+
+    // step 5
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+    // step 6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+    //step 7
+    const newBalance = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalance;
 })
